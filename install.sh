5ce657c6cf08cb660c9ab014a7f0e5cd3456a4b6
QA Automation <44262112+konflic@users.noreply.github.com>
2020-10-18 15:13:45 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python3 -m venv venv
-source venv/bin/activate
-pip isntall -U pip
+python3 -m venv venv && \
+source venv/bin/activate && \
+pip install -U pip && \
 pip install -Ur requirements.txt
